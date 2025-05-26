c27dee3c089a566dce30a20d5a5c4216405ff284
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2025-05-26 20:42:04 -0300

    ajustes de quebra de linha

--- principal.cpp
+++ principal.cpp
@@ -26,22 +26,22 @@ int main()
     cout << "\nElementos da lista B:\n";
     exibir(listaB);
 
-    cout << "\nRemovendo os elementos pares da listaA\n\n";
+    cout << "\n\nRemovendo os elementos pares da listaA\n";
     listaA = removePares(listaA);
 
     // unir listas
-    cout << "unindo listas...\n\n";
+    cout << "\nunindo listas...\n";
     Celula *listaUnificada = NULL;
     listaUnificada = unirListas(listaA, listaB);
     exibir(listaUnificada);
 
     // ordenar lista unificada
     ordenarBolha(listaUnificada);
-    cout << "\nExibindo lista unificada ordenada:\n";   
+    cout << "\n\nExibindo lista unificada ordenada:\n";   
     exibir(listaUnificada);
 
     // exclusao lista unificada
-    cout << "\nExcluindo elementos da lista unificada ordenada...\n";   
+    cout << "\n\nExcluindo elementos da lista unificada ordenada...\n";   
     listaUnificada = removerTodos(listaUnificada);
     cout << "\nExibindo lista unificada ordenada apos delesao:\n";  
     exibir(listaUnificada);
